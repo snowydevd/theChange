03e1c8db434eaae26c42e9cc713debe9ee5f65a1
Snowie Dev <lautarochini4@gmail.com>
2023-10-11 19:43:11 -0300

    link upgrade

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="theChange" />
+        <meta
+          property="og:image"
+          content="https://cdn.discordapp.com/attachments/1153719599746007040/1161796064521756732/theChange.png?ex=653999e6&is=652724e6&hm=a2c56f48f76c74abf179f410a77ab5bc4587831eed9aac546705e271f5330320&"
+        />
+        <meta property="og:description" content="Make the change" />
+        <meta property="og:url" content="https://the-change.vercel.app" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
